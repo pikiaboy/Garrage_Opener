56d814e003d14ead2ccc48f45c93207762e40b73
Thomas Pham <thomas.pham96@yahoo.com>
2017-07-16 04:03:58 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 webiopi().ready(init);
 
 function init(){
+	webiopi().setFunction(4,"OUT");
+	webiopi().setFunction(17,"IN");
 	webiopi().refreshGPIO(true);
-	var x = setInterval(function() { isOpen() }, 500);
-	var y = setInterval(function() { getTime() }, 1000);
+	
+	//Update time every second
+	setInterval(function() { getTime() }, 1000);	
+	
+	//Check every 500ms if garrage is open
+	setInterval(function() { isOpen() }, 500);
+	
+	//Auto-Close door
+	setInterval(function() { autoClose() }, 1000);
+}
+
+function autoClose(){
+	var d = new Date();	
+	
+	if(d.getHours() > 22 || d.getHours() < 5 && isOpen())
+	{
+		toggle();
+	}
+			
+
+
 }
 
 //Check to see if Garrage is open
 function isOpen(){
-	if(webiopi().digitalRead(4) == 0)
-		document.getElementById("status").innerText = "Closed";
+	if(webiopi().digitalRead(17) == 0)
+		{
+			document.getElementById("status").innerText = "Closed";
+			return false;
+		}
 	else
-		document.getElementById("status").innerText= "Open";
+		{
+			document.getElementById("status").innerText= "Open";
+			return true;
+		}
 }
+
 
 function getTime(){
 	var d = new Date(); 
